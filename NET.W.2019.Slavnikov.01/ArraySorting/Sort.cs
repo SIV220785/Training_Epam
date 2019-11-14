5abb64ef9b26f095257d5821fc246e7d887f2add
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-11-14 21:54:16 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.01/ArraySorting/Sort.cs
@@ -0,0 +1,131 @@
+﻿namespace ArraySorting
+{
+    public static class Sort
+    {
+        /// <summary>
+        /// QuaickSorts
+        /// </summary>
+        /// <param name="array">Array of numbers</param>
+        public static void QuaickSort(this int[] array)
+        {
+            if (array == null)
+            {
+                return;
+            }
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        /// <summary>
+        /// Recursive sorting of each part of the array
+        /// </summary>
+        /// <param name="array">Array of numbers</param>
+        /// <param name="start">Start index</param>
+        /// <param name="end">End index</param>
+        public static void QuickSort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                int pivot = Partition(array, start, end);
+                QuickSort(array, start, pivot - 1);
+                QuickSort(array, pivot + 1, end);
+            }
+        }
+
+        /// <summary>
+        /// Partitioning an array relative to a support element
+        /// </summary>
+        /// <param name="array">Array of numbers</param>
+        /// <param name="start">Start index</param>
+        /// <param name="end">End index</param>
+        /// <returns></returns>
+        public static int Partition(int[] array, int start, int end)
+        {
+            int pivot = array[end];
+            int pIndex = start;
+
+            for (int i = start; i < end; i++)
+            {
+                if (array[i] <= pivot)
+                {
+                    int temp = array[i];
+                    array[i] = array[pIndex];
+                    array[pIndex] = temp;
+                    pIndex++;
+                }
+            }
+
+            int anotherTemp = array[pIndex];
+            array[pIndex] = array[end];
+            array[end] = anotherTemp;
+            return pIndex;
+        }
+
+        /// <summary>
+        /// MergeSort
+        /// </summary>
+        /// <param name="array">Array of numbers</param>
+        /// <returns></returns>
+        public static void MergeSort(this int[] array)
+        {
+            if (array.Length <= 1)
+                return;
+            int midPoint = array.Length / 2;
+            int[] leftArr = new int[midPoint];
+            int[] rightArr = new int[array.Length - midPoint];
+
+            for (int i = 0; i <= midPoint - 1; i++)
+            {
+                leftArr[i] = array[i];
+            }
+            for (int i = midPoint; i <= array.Length - 1; i++)
+            {
+                rightArr[i - midPoint] = array[i];
+            }
+
+            MergeSort(leftArr);
+            MergeSort(rightArr);
+            Merge(array, leftArr, rightArr);
+        }
+
+        /// <summary>
+        /// Combining subarrays into one sorted array
+        /// </summary>
+        /// <param name="array">Array to sort</param>
+        /// <param name="leftArr">Left subarray</param>
+        /// <param name="rightArr">Right subarray</param>
+        public static void Merge(int[] array, int[] leftArr, int[] rightArr)
+        {
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < leftArr.Length && j < rightArr.Length)
+            {
+                if (leftArr[i] <= rightArr[j])
+                {
+                    array[k] = leftArr[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArr[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < leftArr.Length)
+            {
+                array[k] = leftArr[i];
+                i++;
+                k++;
+            }
+            while (j < rightArr.Length)
+            {
+                array[k] = rightArr[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
---
+++ NET.W.2019.Slavnikov.01/QuickSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using ArraySorting;
+using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 6, 4, 2, 5, 7 };
+            int[] arr2 = new int[8] { 6, 4, 2, 5, 7, 0, 1, 3 };
+
+            arr.QuaickSort();
+            arr2.MergeSort();
+
+            Console.WriteLine("QuackSort:");
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("MergeSort:");
+
+            foreach (var item in arr2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+
+
+}
