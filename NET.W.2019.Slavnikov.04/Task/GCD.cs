f451d55a8e076aa09e9d630d95a32b649f979d67
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-11-21 22:56:19 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.04/Task/GCD.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Task
+{
+    public class GCD
+    {
+        /// <summary>
+        /// Bit-insert of number from i to j position
+        /// </summary>
+        /// <param name="numbers">Array of source numbers</param>
+        /// <param name="execTime">Executing Time spending on GCD finding</param>
+        /// <returns>
+        /// Returns GCD by Euclidean Algorithm
+        /// </returns>
+        public static int EuclideanGCD(out double execTime, params int[] numbers)
+        {
+            if (numbers.Length == 0) throw new ArgumentNullException("No params");
+            if (numbers.Contains(0)) numbers.ToList().RemoveAll(n => n == 0);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int res = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                res = EuclideanAlgorithm(Math.Abs(numbers[i]), Math.Abs(numbers[i + 1]));
+                numbers[i + 1] = res;
+            }
+
+            stopwatch.Stop();
+
+            TimeSpan timeDiff = stopwatch.Elapsed;
+            execTime = timeDiff.TotalMilliseconds;
+
+            return res;
+        }
+
+        public static int BinaryGCD(out double execTime, params int[] numbers)
+        {
+            if (numbers.Length == 0) throw new ArgumentNullException("No params");
+            if (numbers.Contains(0)) numbers.ToList().RemoveAll(n => n == 0);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int res = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                res = BinaryAlgorithm(Math.Abs(numbers[i]), Math.Abs(numbers[i + 1]));
+                numbers[i + 1] = res;
+            }
+            stopwatch.Stop();
+
+            TimeSpan timeDiff = stopwatch.Elapsed;
+            execTime = timeDiff.TotalMilliseconds;
+
+            return res;
+        }
+
+        private static int EuclideanAlgorithm(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+
+            return a;
+        }
+
+        private static int BinaryAlgorithm(int a, int b)
+        {
+
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            int count = 0;
+
+            while (((a | b) & 1) == 0)
+            {
+                count++;
+                a >>= 1;
+                b >>= 1;
+            }
+
+            while ((a & 1) == 0)
+            {
+                a >>= 1;
+            }
+
+            do
+            {
+                while ((b & 1) == 0)
+                {
+                    b >>= 1;
+                }
+
+                if (a > b)
+                {
+                    (a, b) = (b, a);
+                }
+
+                b = b - a;
+            } while (b != 0);
+
+            return a << count;
+        }
+    }
+}
---
+++ NET.W.2019.Slavnikov.04/Test/Tests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [TestCase(27, 0, 54, ExpectedResult = 27)]
+        [TestCase(120, 18, ExpectedResult = 6)]
+        public int GCDEuclideanCount(params int[] nums)
+        {
+            double execTime = 0;
+            return GCD.EuclideanGCD(out execTime, nums);
+        }
+
+        [Test]
+        [TestCase(27, 0, 54, ExpectedResult = 27)]
+        [TestCase(120, 18, ExpectedResult = 6)]
+        public int GCDBinaryCount(params int[] nums)
+        {
+            double execTime = 0;
+            return GCD.BinaryGCD(out execTime, nums);
+        }
+
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000001")]
+        [TestCase(double.NaN, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.NegativeInfinity, ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.PositiveInfinity, ExpectedResult = "0111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(-0.0, ExpectedResult = "1000000000000000000000000000000000000000000000000000000000000000")]
+        [TestCase(0.0, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000000")]
+        public string DoubleToBinaryStringTest(double number) => NumberRepresentationConverter.DoubleToBinaryString(number);
+    }
+}
