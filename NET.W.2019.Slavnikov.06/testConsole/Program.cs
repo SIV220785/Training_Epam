80cb94efc629f1989a1dee1e030c2666d54beeb4
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-11-28 23:24:33 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.06/testConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 2 }, new[] { -10, 5, 20, 20 } };
+
+
+            SortArray sortArray = new SortArray(array);
+            sortArray.SortDescendingRowBySum();
+            PrinArray(sortArray);
+            Console.WriteLine();
+            sortArray.SortAscendingRowBySum();
+            PrinArray(sortArray);
+            Console.WriteLine();
+            sortArray.SortAscendingByMaxElemRow();
+            PrinArray(sortArray);
+            Console.WriteLine();
+            sortArray.SortDescendingByMaxElemRow();
+            PrinArray(sortArray);
+            Console.WriteLine();
+            sortArray.SortAscendingByMinElemRow();
+            PrinArray(sortArray);
+            Console.WriteLine();
+            sortArray.SortDescendingByMinElemRow();
+            PrinArray(sortArray);
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void PrinArray(SortArray sortArray)
+        {
+            string tmpStr = "";
+            foreach (var item in sortArray.Matrix)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    tmpStr += $"{item[i].ToString()} ";
+                }
+                Console.WriteLine(tmpStr);
+                tmpStr = "";
+            }
+        }
+    }
+}
