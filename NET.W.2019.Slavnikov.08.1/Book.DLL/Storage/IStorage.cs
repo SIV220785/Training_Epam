e697f4c4a1f41eb39e7899dba3f0d82cb0120d25
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-12-04 00:31:06 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.08.1/Book.DLL/Storage/IStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Book.DLL.Entities;
+
+namespace Book.DLL.Storage
+{
+    /// <summary>
+    /// interface IStorage.
+    /// </summary>
+    public interface IStorage
+    {
+        /// <summary>
+        /// Method save to storage.
+        /// </summary>
+        /// <param name="books"> Collection to save.</param>
+        void Save(IEnumerable<BookInfo> books);
+
+        /// <summary>
+        /// Methot get collection from storage.
+        /// </summary>
+        /// <returns>Collection from storage.</returns>
+        IEnumerable<BookInfo> Load();
+    }
+}
