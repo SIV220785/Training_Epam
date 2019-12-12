0d04f089fd6579a2d9d8ac669ea3e80aafc15ab9
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-12-12 23:35:58 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.12/CountdownTimer/Program.cs
@@ -0,0 +1,21 @@
+﻿using Countdown.DLL;
+
+
+namespace Console.PL
+{
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Countdown.DLL.Countdown countdown = new Countdown.DLL.Countdown();
+            CountdownSubscriber countdownSubscriber = new CountdownSubscriber();
+
+            countdownSubscriber.Register(countdown);
+
+            countdown.StartCountdown(10, "lya,lya");
+            Console.WriteLine("++");
+            Console.ReadKey();
+        }
+    }
+}
