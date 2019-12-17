98fc0e7d20a4f5503202cc2214dcd5948e64eb96
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-12-17 21:25:18 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.13/ConsoleQueue.PL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleQueue.PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue.DLL.Queue<int> queue = new CustomQueue.DLL.Queue<int>(5);
+
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(queue.Peek());
+            queue.Dequeue();
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
