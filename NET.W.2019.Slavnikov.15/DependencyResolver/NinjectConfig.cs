b3c6d9f7d7de09489192185e1511dc424ad715bd
SIV220785 <55081261+SIV220785@users.noreply.github.com>
2019-12-26 07:56:16 +0300

    Add files via upload

---
+++ NET.W.2019.Slavnikov.15/DependencyResolver/NinjectConfig.cs
@@ -0,0 +1,19 @@
+﻿using Bank.BLL.Service.Bank;
+using Bank.BLL.Service.Base;
+using Bank.BLL.Service.GenerationId;
+using Bank.DAL.Contexts;
+using Ninject;
+using System;
+
+namespace DependencyResolver
+{
+    public static class NinjectConfig
+    {
+        public static void ConfigurateResolver(this IKernel kernel)
+        {
+            kernel.Bind<IBank>().To<BaseBank>().InSingletonScope();;
+            kernel.Bind<IGenerationIdAccount>().To<GenerationIdAccount>().InSingletonScope();
+            kernel.Bind<BankContext>().To<BankContext>();
+        }
+    }
+}
